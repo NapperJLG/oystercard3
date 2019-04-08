1ce9a949e126efb8a5d788353993d3d8418144d2
Jake Napper <napperJLG@gmail.com>
2019-04-08 16:52:15 +0100

    card can now  be topped up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,13 @@ require 'oystercard'
 
 describe Oystercard do
   it "checks new card has a balance of 0" do
-    card = Oystercard.new
     expect(subject.balance).to eq 0
+  end
+
+  describe '#top_up' do
+    it 'tops up balance of the card by a specified amount' do
+      subject.top_up(30)
+      expect(subject.balance).to eq 30
+    end
   end
 end
